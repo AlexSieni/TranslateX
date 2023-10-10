7b75c8745bfdd3e92f2d275e5c81654c0ba7aa10
Sean Blizard <emailseanblizard@gmail.com>
2023-10-10 15:21:21 -0400

    Installed react and corresponding files

--- test.py
+++
@@ -1,8 +0,0 @@
-import whisper # import openai library
-import csv # import csv libary
-model = whisper.load_model("base") # loads model
-result = model.transcribe("medium.mp3") # audio input into model and returns text 
-f = open('text.csv', 'w') # opens pre-existing csv file using path
-writer = csv.writer(f) # creates writer of csv
-data = [result["text"]] # converts output to different data type for convenience purposes
-writer.writerow(data) # writes data to csv file
